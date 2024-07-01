e766240f133e9a30f83892272362d538cba0d690
Nikhil2005Rai <n2005rai@gmail.com>
2024-07-01 19:06:46 +0530

    Add jwt

--- src/models/user.mode.js
+++ src/models/user.mode.js
@@ -55,9 +55,34 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password);
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
+userSchema.methods.generateAccessToken = async function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = async function () {
+    jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      }
+    );
+};
 
 export const User = mongoose.model("User", userSchema);
