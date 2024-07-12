114067c313ac95a772aeb6f4f6829894b3e3ecc4
Nikhil2005Rai <n2005rai@gmail.com>
2024-07-12 17:18:22 +0530

    Add new controller method changeCurrentPassword

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -223,8 +223,28 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         )
       );
   } catch (error) {
-    throw new ApiError(), error?.message || "invalid refresh token"
+    throw (new ApiError(), error?.message || "invalid refresh token");
   }
+});
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req.user?.id);
+
+  const isPasswordCorrect = user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Ivalid Old Password");
+  }
+
+  user.password = newPassword;
+  await user.save({validateBeforeSave: false})
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, {}, "Password changed Successfully")
+  )
 });
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken };
